4eda1ee62c4ef2cf6fc34a4cb216c8182e3ca5f0
Bernal30 <abrn7311@gmail.com>
2025-08-31 22:39:43 -0600

    wip - Restructuring files, aplication javascript and readme file

--- README.md
+++ README.md
@@ -1,27 +1,42 @@
 <h1 align="center">La Función factorial 🔢📈</h1>
 
 <p align="center">
-    <img src="img/Gamma1.png">
+    <img src="img/web-img/Gamma1.png">
 </p>
 
 En esta pagina se habla un poco de la interesante `función factorial`, desde su definición matematica, un valor curioso como el cero factorial, su extención análitica al plano de los números reales y complejos, también al final hay un apartado para calcular el factorial de un número entero entre 1 y 100 implementando conocimientos de logica de programación.
 
+## Tecnologias 💻
+
+Para este proyecto landing page solo se utilizaron las tres tecnologias principales de un pagina web:
+
+- HTML 5
+- CSS 3
+- JavaScrpit
+
+## Funcionalidades 🔨
+
+### Sección calcular factorial
+
+Al ingresar un número valido para calcular su factorial (un positivo entero o el cero) en pantalla aparecera el resultado. Entre las funcionalidades de los botones puedes dar click en el boton **Calcular** o presionar **Enter** para ver el resultado.
+
+<p align="center">
+    <img src="img/web-interface/factorial-calculation-interface.png">
+</p>
+
+### Logica de los botones
+
+Los botones estaran habilitados bajo su respectiva lógica de uso, así cuando se realice un intento el boton **_Intentar de nuevo_** pasara a estar habilitado mientras que el boton **_Calcular_** se deshabilitara. Estas funcionalidades se logran gracias a la manipulación del atributo `disable` para botones en HTML mediante una lógica en javaScript:
+
+<p align="center">
+    <img src="img/code-screenshots/button-logic.png">
+</p>
+
+### Funcionalidad tecnica addEventListener
+
+
 ## Estado del proyecto 🚧
 
 La pagina web aun está en desarrollo y un amantenimiento para pulir detalles tecnicos.
-
-## Tecnologias 💻
-
-Para este proyecto de pagina web estatica solo se utilizaron las tres tecnologias principales de un pagina web:
-
-- **HTML 5**
-
-- **CSS 3**
-
-- **JavaScrpit**
-
-## Funcionalidades 
-
-
 
 Autor: [Berny](https://github.com/Bernal30) ♦️
--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+//variables y funciones globales
+let againCalculationButton = document.getElementById("reinicio");
+let calculationFactorialButton = document.getElementById("calculo_factorial");
+const factorialInputObject = document.getElementById("valorUsuario");
+
+//se capturan eventos de teclado y mouse para funcionalidades de los botones
+calculationFactorialButton.addEventListener("click", factorialNumeroUsuario);
+againCalculationButton.addEventListener("click", intentarDeNuevo);
+factorialInputObject.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    calculationFactorialButton.click();
+  }
+});
+
 function asignarTextoHTML(etiqueta, texto) {
   let etiquetaHTML = document.getElementById(etiqueta);
   return (etiquetaHTML.innerHTML = texto);
@@ -6,28 +20,50 @@ function asignarTextoHTML(etiqueta, texto) {
 function condicionInicial() {
   asignarTextoHTML(
     "texto-parrafo",
-    `Digite un número entero entre 0 y 100 para calcular su factorial:`
+    "Digite un número entero entre 0 y 100 para calcular su factorial:"
   );
   vaciarInput();
   return;
 }
 
+function vaciarInput() {
+  return (document.getElementById("valorUsuario").value = "");
+}
+
+//calculo del factorial de un número entero positivo o el cero
+function calculoFactorial(number) {
+  if (number === 0) {
+    return number + 1;
+  } else {
+    let resultadoFactorial = number * calculoFactorial(number - 1);
+    return resultadoFactorial;
+  }
+}
+
+//se habilita el boton "Intentar de nuevo" y se deshabilita el boton "Calcular"
+function buttonLogic() {
+  document.getElementById("reinicio").removeAttribute("disabled");
+  document.getElementById("calculo_factorial").setAttribute("disabled", "true");
+  return;
+}
+
+//se llama a esta función mediante el boton intentar de nuevo
+function intentarDeNuevo() {
+  //deshabilitar el boton intentar de nuevo
+  document.getElementById("reinicio").setAttribute("disabled", "true");
+  //activar el boton calcular
+  document.getElementById("calculo_factorial").removeAttribute("disabled");
+  condicionInicial();
+  return;
+}
+
 condicionInicial();
 
-/*
-function multiplicacion() {
-    let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);    
-    let resultado = (numeroUsuario)*2;
-    return console.log(resultado);     
-}
-*/
-
-//se muestra en pantalla el resultado del factorial del número del usuario en caso de que sea un entero positivo entre 1 y 100
+//se muestra en pantalla el resultado del factorial, antes se evalua si el número es valido
 function factorialNumeroUsuario() {
   let numeroUsuario = parseInt(document.getElementById("valorUsuario").value);
   vaciarInput();
-  activarBotonReinicio();
-  apagarBotonCalcular();
+  buttonLogic();
   if (numeroUsuario > 100) {
     return asignarTextoHTML(
       "texto-parrafo",
@@ -49,51 +85,3 @@ function factorialNumeroUsuario() {
     }
   }
 }
-
-let calculationFactorialButton = document.getElementById("calculo_factorial");
-//cuando se da clic en el boton "Calcular"
-calculationFactorialButton.onclick = function () {
-  return factorialNumeroUsuario();
-};
-
-//calculo del factorial de un número entero positivo o el cero
-function calculoFactorial(number) {
-  if (number === 0) {
-    return number + 1;
-  } else {
-    let resultadoFactorial = number * calculoFactorial(number - 1);
-    return resultadoFactorial;
-  }
-}
-
-function vaciarInput() {
-  return (document.getElementById("valorUsuario").value = "");
-}
-
-//se habilita el boton "Intentar de nuevo"
-function activarBotonReinicio() {
-  return document.getElementById("reinicio").removeAttribute("disabled");
-}
-
-//se deshabilita el boton "Calcular"
-function apagarBotonCalcular() {
-  document.getElementById("calculo_factorial").setAttribute("disabled", "true");
-  return;
-}
-
-//se llama a esta función mediante el boton intentar de nuevo
-function intentarDeNuevo() {
-  //deshabilitar el boton intentar de nuevo
-  document.getElementById("reinicio").setAttribute("disabled", "true");
-  //volver a la condicion inicial
-  condicionInicial();
-  //activar el boton calcular
-  document.getElementById("calculo_factorial").removeAttribute("disabled");
-  return;
-}
-
-let againCalculationButton = document.getElementById("reinicio");
-//cuando se da clic en el boton "Intentar de nuevo"
-againCalculationButton.onclick = function () {
-  return intentarDeNuevo();
-};
